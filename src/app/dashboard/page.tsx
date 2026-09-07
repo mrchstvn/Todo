96d68e8c205b7d5d151dc42294cc29ef1765c4b0
b4pnu <mrchtzy@gmail.com>
2026-09-07 23:28:49 +0800

    to be continued tomorrow

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import LogoutButton from "@/components/ui/logout-button";
+import SideNav from "@/components/layout/side-nav";
 
 export default function DashBoardPage() {
   return (
-    <div>
-      <h1>Hello from DashBoardPage</h1>
-      <LogoutButton />
+    <div className="flex">
+      <SideNav />
+      <div className="p-4">
+        <h1>Hello from DashBoardPage</h1>
+        <LogoutButton />
+      </div>
     </div>
   );
 }
---
+++ src/components/layout/side-nav.tsx
@@ -0,0 +1,13 @@
+export default function SideNav() {
+  return (
+    <div className="flex flex-col w-64 h-screen bg-gray-50 shadow-sm">
+      <div className="flex items-center justify-between h-16">
+        <h1>LOGO</h1>
+        <div> = </div>
+      </div>
+      <div>
+        <p>Tasks</p>
+      </div>
+    </div>
+  );
+}
