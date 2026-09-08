74663fcf44a5c461b548c248c7277306c7199b1a
mrchtzy <mrchstvn@gmail.com>
2026-09-08 16:40:35 +0800

    sidebar not finished yet

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,20 @@
 import LogoutButton from "@/components/ui/logout-button";
-import SideNav from "@/components/layout/side-nav";
+import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function DashBoardPage() {
   return (
     <div className="flex">
-      <SideNav />
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "20rem",
+            "--sidebar-width-mobile": "20rem",
+          } as React.CSSProperties
+        }
+      >
+        <Sidebar />
+      </SidebarProvider>
+
       <div className="p-4">
         <h1>Hello from DashBoardPage</h1>
         <LogoutButton />
